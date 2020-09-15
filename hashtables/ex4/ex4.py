ab19bb6b378f429cd0eb40c66b7b259c2a6316c6
CScori <49799016+CScori@users.noreply.github.com>
2020-09-14 18:55:10 -0700

    old territory 1

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,28 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    crossover = {}
+
+    for number in a:
+        absolute = abs(number)
+        if absolute not in crossover:
+            crossover[absolute] = 1
+        else: 
+            crossover[absolute] += 1
+
+    expects = {} 
+  
+    for key, value in crossover.items(): 
+        if value not in expects: 
+            expects[value] = [key] 
+        else: 
+            expects[value].append(key) 
+
+    result = []
+
+    if 2 in expects:
+        result = expects[2]
 
     return result
+
 
 
 if __name__ == "__main__":
