db66d18c3a105412eb11e7ae37a695ffb0e52478
CScori <49799016+CScori@users.noreply.github.com>
2020-09-14 19:31:35 -0700

    donezo

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,21 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    ticket_hash = {}
+    route = []
+    
+    for tix in tickets:
+        if tix.source == "NONE":
+            route.append(tix.destination)
+        ticket_hash[tix.source] = tix.destination
+        
+    current_tix = ticket_hash[route[0]]
+    
+    count = 1
+    
+    while count < length:
+        route.append(current_tix)
+        current_tix = ticket_hash[current_tix]
+        count +=1
 
     return route
